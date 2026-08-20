4edc4dd72c582677b16bbe544e039fc67a475915
ItzTom10 <itzel.torres@transparencia.gob.mx>
2026-08-20 02:19:30 +0000

    S07

---
+++ S03/Main.java
@@ -0,0 +1,20 @@
+package S03;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        // Objetos
+        Persona p1 = new Persona("Carlos", 27);
+        Persona p2 = new Persona("Patricia", 20);
+        Persona p3 = new Persona("Brenda", 22);
+
+
+        // Resultados
+        p1.presentarse();
+        p2.presentarse();
+        p3.presentarse();
+    }
+}
